6ad481713a46ea0a89d75b1954aec1c4c9a4f046
jackwalten <jackwalten@ideapad>
2024-04-30 18:58:32 +0530

    Updated neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,3 +24,83 @@ vim.opt.updatetime = 50
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ 'r', 'o' })
+  end,
+})
+
+-- Netrw
+vim.g.netrw_banner = 0
+
+-- Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     config = function()
+    --         require("tokyonight").setup({
+    --             styles = {
+    --                 comments = { italic = false },
+    --                 keywords = { italic = false },
+    --             },
+    --             transparent = true,
+    --         })
+    --         vim.cmd.colorscheme("tokyonight-night")
+    --     end,
+    -- },
+    {
+        "rose-pine/neovim",
+        config = function()
+            require("rose-pine").setup({
+                styles = {
+                    italic = false,
+                    transparency = true,
+                },
+            })
+            vim.cmd.colorscheme("rose-pine")
+        end,
+    },
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
+        lazy = false,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "cpp", "lua", "python", "vim", "vimdoc", "query" },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+    {
+        "echasnovski/mini.statusline", version = false,
+        config = function()
+            require("mini.statusline").setup()
+        end,
+    },
+})
